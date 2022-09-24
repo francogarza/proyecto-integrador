984d8f81123e2031912b7b8f6fec6bd9eaa5dbc3
psalazartec <43385170+psalazartec@users.noreply.github.com>
2022-09-23 19:24:23 -0500

    Registro Taller
    Se creo la pagina de registro de Taller

---
+++ src/RegistroTalleres.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import {db} from './firebase';
+import {uid} from 'uid';
+import {set, ref} from 'firebase/database';
+import {useState} from "react";
+
+const RegistroTalleres = () => {
+
+    const [Descripcion, setDescripcion] = useState("");
+    const [Fechas, setFechas] = useState("");
+    const [Horarios, setHorarios] = useState("");
+    const [ImpartidoPor, setImpartidoPor] = useState("");
+    const [Nombre, setNombre] = useState("");
+    const [Prerequisitos, setPrerequisitos] = useState("");
+    const [VirtualPresencial, setVirtualPresencial] = useState("");
+
+    const handleChangeDescripcion=(e)=>{
+        setDescripcion(e.target.value)
+    }
+
+    const handleChangeFechas=(e)=>{
+        setFechas(e.target.value)
+    }
+
+    const handleChangeHorarios=(e)=>{
+        setHorarios(e.target.value)
+    }
+
+    const handleChangeImpartidoPor=(e)=>{
+        setImpartidoPor(e.target.value)
+    }
+
+    const handleChangeNombre=(e)=>{
+        setNombre(e.target.value)
+    }
+
+    const handleChangePrerequisitos=(e)=>{
+        setPrerequisitos(e.target.value)
+    }
+    const handleChangeVirtualPresencial=(e)=>{
+        setVirtualPresencial(e.target.value)
+    }
+
+    const writeToDatabase = () => {
+        const uuid = uid()
+        set(ref(db, 'Taller/patotest/'+ uuid), {
+            Descripcion,
+            Fechas,
+            Horarios,
+            ImpartidoPor,
+            Nombre,
+            Prerequisitos,
+            VirtualPresencial,
+        });
+        setDescripcion("");
+        setFechas("");
+        setHorarios("");
+        setImpartidoPor("");
+        setNombre("");
+        setPrerequisitos("");
+        setVirtualPresencial("");
+    };
+
+    return(
+        <form classname="registroTaller">
+            <label>
+                Escriba la descripcion del taller.
+            </label>
+            <br/>
+            <input type="text" placeholder="Descripcion" id="Descripcion" value={Descripcion} onChange={handleChangeDescripcion} required="true"/>
+            <br/>
+            <br/>
+            <label>
+                Seleccione la fecha de inicio del Taller.
+            </label>
+            <br/>
+            <input type="date" id="Fechas" value={Fechas} onChange={handleChangeFechas} required="true"/>
+            <br/>
+            <br/>
+            <label>
+                Escribe el nombre de quien imparte el taller.
+            </label>
+            <br/>
+            <input type="text" placeholder="Nombre" id="ImpartidoPor" value={ImpartidoPor} onChange={handleChangeImpartidoPor} required="true"/>
+            <br/>
+            <br/>
+            <label>
+                Escriba el nombre del Taller.
+            </label>
+            <br/>
+            <input type="text" placeholder="Nombre" id="Nombre" value={Nombre} onChange={handleChangeNombre} required="true"/>
+            <br/>
+            <br/>
+            <label>
+                Esriba los prerequisitos del taller.
+            </label>
+            <br/>
+            <input type="text" placeholder="" id="Prerequisitos" value={Prerequisitos} onChange={handleChangePrerequisitos} required="true"/>
+            <br/>
+            <br/>
+            <label>
+                Seleccione si el taller es virtual o presencial.
+            </label>
+            <br/>
+            <input type="radio" id="Virtual" name="VirtualPresencial" value={VirtualPresencial} onChange={handleChangeVirtualPresencial}
+                   checked="true"/>
+            <label htmlFor="Virtual">Virtual</label>
+            <input type="radio" id="Presencial" name="Presencial" value={VirtualPresencial} onChange={handleChangeVirtualPresencial}/>
+            <label htmlFor="Presencial">Presencial</label>
+            <br/>
+            <br/>
+            <button onClick={writeToDatabase} class="registro" type="submit">
+                Registrar Taller
+            </button>
+        </form>
+    )
+}
+
+export default RegistroTalleres
