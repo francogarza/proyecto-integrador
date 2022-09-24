f0af2bd2ce8aab185418efd98d4c687f497d5908
psalazartec <43385170+psalazartec@users.noreply.github.com>
2022-09-23 19:28:15 -0500

    Registro Talleres en Index
    Registro Talleres se agrego al index para comprobar funcionalidad.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import RegistroPadres from './RegistroPadres.js';
 import RegistroTaller from './RegistroTaller.js';
+import RegistroTalleres from './RegistroTalleres.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     <App />
     <RegistroPadres />
     <RegistroTaller />
+    <RegistroTalleres />
   </React.StrictMode>
 );
 
